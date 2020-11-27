11243f81d02b3bb9a176a83ffd33f7c21094f3bf
gdaemoni <kon4ena.rega3.0@gmail.com>
2020-11-27 14:55:01 +0300

    checkout

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,8 +1,15 @@
 #!groovy
 
-def call(def url, def branch = "master", def credentialsId = "") {
+def call(body) {
+	def config = [:]
+	
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+
 	echo "texxxxt"
-	git url: url,
-		branch: branch,
-		credentialsId: credentialsId
+	
+	git url: config.url,
+		branch: config.branch,
+		credentialsId: config.credentialsId
 }

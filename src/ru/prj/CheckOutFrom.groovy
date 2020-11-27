e21da4f25a97f9ff720193297c1f5045c3a6439e
gdaemoni <kon4ena.rega3.0@gmail.com>
2020-11-27 14:43:25 +0300

    checkout

--- src/ru/prj/CheckOutFrom.groovy
+++ src/ru/prj/CheckOutFrom.groovy
@@ -1,8 +1,11 @@
-#!groovy
+// src/ru/prj/CheckOutFrom.groovy
+package ru.prj
 
+class CheckOutFrom {
 def call(def url, def branch = "master", def credentialsId = "") {
 	echo "texxxxt"
 	git url: url,
 		branch: branch,
 		credentialsId: credentialsId
+}	
 }

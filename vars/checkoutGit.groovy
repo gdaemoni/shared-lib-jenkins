020032a54189ea96c682ae40436ebb3bffd361dd
gdaemoni <kon4ena.rega3.0@gmail.com>
2020-11-27 12:06:14 +0300

    checkout

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,14 +1,12 @@
 #!groovy
 
 def call(def url, def branch = "master", def credentialsId = "") {
-	return {
-		stage ("checkout ${br}") {
-			steps {
-				echo "texxxxt"
-				git url: url
-					branch: branch
-					credentialsId: credentialsId
-			}
+	stage ("checkout ${br}") {
+		steps {
+			echo "texxxxt"
+			git url: url
+				branch: branch
+				credentialsId: credentialsId
 		}
 	}
 }

816ad6f37ee49b94b0ec6915636704b0f0cfbd7c
gdaemoni <kon4ena.rega3.0@gmail.com>
2020-12-01 16:15:36 +0300

    commit

---
+++ vars/piplineTest.groovy
@@ -0,0 +1,34 @@
+#!groovy
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+
+	stage {
+		echo pipelineParams.branch
+	}
+	// pipeline {
+    // agent any
+
+	// stages {
+	// 	stage('checkout') {
+	// 		steps {
+	// 			git url: config.url,
+	// 				branch: config.branch,
+	// 				credentialsId: config.credentialsId
+	// 		}
+	// 	}
+    // 	stage('Build') {
+	// 		steps {
+	// 			script {
+	// 				echo "test"
+    //     		}
+    //     	}
+    // 	}
+    // }
+  }
+}
